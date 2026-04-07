52dce12a0f3408fd677c002f1e8b2d57d7665b79
Saskia Steyn <u17267162@tuks.co.za>
2026-04-07 21:26:13 +0200

    feat: added global styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.scss';
+
+const themeColors = [
+    '#f2fbe8',
+    '#e6f3d9',
+    '#cde4b6',
+    '#b3d48f',
+    '#9cc76e',
+    '#8dbf59',
+    '#7db344',
+    '#72a53d',
+    '#649234',
+    '#547f27',
+];
+
+const theme = createTheme({
+    colors: {
+        themeColors,
+    },
+    forceColorScheme: 'light',
+    fontFamily: "'Roboto', sans-serif",
+    headings: {
+        fontFamily: "'Roboto', sans-serif",
+    },
+    primaryColor: 'themeColors',
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
+    </StrictMode>,
+);
